56d5c223025baa2969e47ae5e0cd61cb7950e051
José M. Giménez-García <jm.gimenez.garcia@gmail.com>
2018-06-19 11:44:55 +0200

    latest changes

---
+++ hdt-java-core/src/main/java/org/rdfhdt/hdt/dictionary/impl/GraphsFourSectionDictionaryBig.java
@@ -0,0 +1,171 @@
+/**
+ * File: $HeadURL: https://hdt-java.googlecode.com/svn/trunk/hdt-java/src/org/rdfhdt/hdt/dictionary/impl/FourSectionDictionary.java $
+ * Revision: $Rev: 191 $
+ * Last modified: $Date: 2013-03-03 11:41:43 +0000 (dom, 03 mar 2013) $
+ * Last modified by: $Author: mario.arias $
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Contacting the authors:
+ * Mario Arias: mario.arias@deri.org
+ * Javier D. Fernandez: jfergar@infor.uva.es
+ * Miguel A. Martinez-Prieto: migumar2@infor.uva.es
+ * Alejandro Andres: fuzzy.alej@gmail.com
+ */
+
+package org.rdfhdt.hdt.dictionary.impl;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.rdfhdt.hdt.dictionary.DictionarySectionPrivate;
+import org.rdfhdt.hdt.dictionary.GraphsDictionary;
+import org.rdfhdt.hdt.dictionary.impl.section.DictionarySectionFactory;
+import org.rdfhdt.hdt.dictionary.impl.section.PFCDictionarySectionBig;
+import org.rdfhdt.hdt.exceptions.IllegalFormatException;
+import org.rdfhdt.hdt.hdt.HDTVocabulary;
+import org.rdfhdt.hdt.header.Header;
+import org.rdfhdt.hdt.listener.ProgressListener;
+import org.rdfhdt.hdt.options.ControlInfo;
+import org.rdfhdt.hdt.options.ControlInfo.Type;
+import org.rdfhdt.hdt.options.ControlInformation;
+import org.rdfhdt.hdt.options.HDTOptions;
+import org.rdfhdt.hdt.util.io.CountInputStream;
+import org.rdfhdt.hdt.util.listener.IntermediateListener;
+
+/**
+ * @author mario.arias,Lyudmila Balakireva, José M. Giménez-García
+ *
+ */
+public class GraphsFourSectionDictionaryBig extends BaseGraphsDictionary {
+
+    public GraphsFourSectionDictionaryBig(final HDTOptions spec,
+	    final DictionarySectionPrivate s, final DictionarySectionPrivate o, final DictionarySectionPrivate sh, final DictionarySectionPrivate g) {
+	super(spec);
+	this.subjects = s;
+	this.objects = o;
+	this.shared = sh;
+	this.graphs = g;
+    }
+
+    public GraphsFourSectionDictionaryBig(final HDTOptions spec) {
+	super(spec);
+	// FIXME: Read type from spec.
+	this.subjects = new PFCDictionarySectionBig(spec);
+	this.objects = new PFCDictionarySectionBig(spec);
+	this.shared = new PFCDictionarySectionBig(spec);
+	this.graphs = new PFCDictionarySectionBig(spec);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see hdt.dictionary.Dictionary#load(hdt.dictionary.Dictionary)
+     */
+    @Override
+    public void load(final GraphsDictionary other, final ProgressListener listener) {
+	final IntermediateListener iListener = new IntermediateListener(listener);
+	this.subjects.load(other.getSubjects(), iListener);
+	this.objects.load(other.getObjects(), iListener);
+	this.shared.load(other.getShared(), iListener);
+	this.graphs.load(other.getGraphs(), iListener);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see hdt.dictionary.Dictionary#save(java.io.OutputStream, hdt.ControlInformation, hdt.ProgressListener)
+     */
+    @Override
+    public void save(final OutputStream output, final ControlInfo ci, final ProgressListener listener) throws IOException {
+	ci.setType(Type.DICTIONARY);
+	ci.setFormat(HDTVocabulary.DICTIONARY_TYPE_FOUR_SECTION);
+	ci.setInt("elements", this.getNumberOfElements());
+	ci.save(output);
+
+	final IntermediateListener iListener = new IntermediateListener(listener);
+	this.shared.save(output, iListener);
+	this.subjects.save(output, iListener);
+	this.objects.save(output, iListener);
+	this.graphs.save(output, iListener);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see hdt.dictionary.Dictionary#load(java.io.InputStream)
+     */
+    @Override
+    public void load(final InputStream input, final ControlInfo ci, final ProgressListener listener) throws IOException {
+	if (ci.getType() != ControlInfo.Type.DICTIONARY) { throw new IllegalFormatException("Trying to read a dictionary section, but was not dictionary."); }
+
+	final IntermediateListener iListener = new IntermediateListener(listener);
+
+	this.shared = DictionarySectionFactory.loadFrom(input, iListener);
+	this.subjects = DictionarySectionFactory.loadFrom(input, iListener);
+	this.objects = DictionarySectionFactory.loadFrom(input, iListener);
+	this.graphs = DictionarySectionFactory.loadFrom(input, iListener);
+    }
+
+    @Override
+    public void mapFromFile(final CountInputStream in, final File f, final ProgressListener listener) throws IOException {
+	final ControlInformation ci = new ControlInformation();
+	ci.load(in);
+	if (ci.getType() != ControlInfo.Type.DICTIONARY) { throw new IllegalFormatException("Trying to read a dictionary section, but was not dictionary."); }
+
+	final IntermediateListener iListener = new IntermediateListener(listener);
+	this.shared = DictionarySectionFactory.loadFrom(in, f, iListener);
+	this.subjects = DictionarySectionFactory.loadFrom(in, f, iListener);
+	this.objects = DictionarySectionFactory.loadFrom(in, f, iListener);
+	this.graphs = DictionarySectionFactory.loadFrom(in, f, iListener);
+
+	// Use cache only for predicates. Preload only up to 100K predicates.
+	// FIXME: DISABLED
+	// predicates = new DictionarySectionCacheAll(predicates, predicates.getNumberOfElements()<100000);
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see hdt.dictionary.Dictionary#populateHeader(hdt.header.Header, java.lang.String)
+     */
+    @Override
+    public void populateHeader(final Header header, final String rootNode) {
+	header.insert(rootNode, HDTVocabulary.DICTIONARY_TYPE, HDTVocabulary.DICTIONARY_TYPE_FOUR_SECTION);
+	header.insert(rootNode, HDTVocabulary.DICTIONARY_NUMSUBJECTS, this.getNsubjects());
+	header.insert(rootNode, HDTVocabulary.DICTIONARY_NUMOBJECTS, this.getNobjects());
+	header.insert(rootNode, HDTVocabulary.DICTIONARY_NUMSHARED, this.getNshared());
+	header.insert(rootNode, HDTVocabulary.DICTIONARY_NUMGRAPHS, this.getNgraphs());
+	// header.insert(rootNode, HDTVocabulary.DICTIONARY_MAXSUBJECTID, getMaxSubjectID());
+	// header.insert(rootNode, HDTVocabulary.DICTIONARY_MAXPREDICATEID, getMaxPredicateID());
+	// header.insert(rootNode, HDTVocabulary.DICTIONARY_MAXOBJECTTID, getMaxObjectID());
+	header.insert(rootNode, HDTVocabulary.DICTIONARY_SIZE_STRINGS, this.size());
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see hdt.dictionary.Dictionary#getType()
+     */
+    @Override
+    public String getType() {
+	return HDTVocabulary.DICTIONARY_TYPE_FOUR_SECTION;
+    }
+
+    @Override
+    public void close() throws IOException {
+	this.shared.close();
+	this.subjects.close();
+	this.objects.close();
+	this.graphs.close();
+    }
+}
---
+++ hdt-java-core/src/main/java/org/rdfhdt/hdt/dictionary/impl/HashGraphsDictionary.java
@@ -0,0 +1,232 @@
+/**
+ * File: $HeadURL: https://hdt-java.googlecode.com/svn/trunk/hdt-java/src/org/rdfhdt/hdt/dictionary/impl/HashDictionary.java $
+ * Revision: $Rev: 191 $
+ * Last modified: $Date: 2013-03-03 11:41:43 +0000 (dom, 03 mar 2013) $
+ * Last modified by: $Author: mario.arias $
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * Contacting the authors:
+ * Mario Arias: mario.arias@deri.org
+ * Javier D. Fernandez: jfergar@infor.uva.es
+ * Miguel A. Martinez-Prieto: migumar2@infor.uva.es
+ * Alejandro Andres: fuzzy.alej@gmail.com
+ */
+
+package org.rdfhdt.hdt.dictionary.impl;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.rdfhdt.hdt.dictionary.impl.section.HashDictionarySection;
+import org.rdfhdt.hdt.enums.TripleComponentRole;
+import org.rdfhdt.hdt.exceptions.NotImplementedException;
+import org.rdfhdt.hdt.options.HDTOptions;
+import org.rdfhdt.hdt.triples.TempTriples;
+import org.rdfhdt.hdt.triples.TripleID;
+import org.rdfhdt.hdt.util.StopWatch;
+
+/**
+ * @author mario.arias, Eugen
+ *
+ */
+public class HashGraphsDictionary extends BaseTempGraphsDictionary {
+
+    public HashGraphsDictionary(final HDTOptions spec) {
+	super(spec);
+
+	// FIXME: Read types from spec
+	this.subjects = new HashDictionarySection();
+	this.objects = new HashDictionarySection();
+	this.shared = new HashDictionarySection();
+	this.graphs = new HashDictionarySection();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see hdt.dictionary.Dictionary#reorganize(hdt.triples.TempTriples)
+     */
+    @Override
+    public void reorganize(final TempTriples triples) {
+	final DictionaryIDMapping mapSubj = new DictionaryIDMapping(this.subjects.getNumberOfElements());
+	final DictionaryIDMapping mapObj = new DictionaryIDMapping(this.objects.getNumberOfElements());
+	final DictionaryIDMapping mapGraph = new DictionaryIDMapping(this.graphs.getNumberOfElements());
+
+	final StopWatch st = new StopWatch();
+
+	// Generate old subject mapping
+	final Iterator<? extends CharSequence> itSubj = this.subjects.getEntries();
+	while (itSubj.hasNext()) {
+	    final CharSequence str = itSubj.next();
+	    mapSubj.add(str);
+
+	    // GENERATE SHARED at the same time
+	    if (str.length() > 0 && str.charAt(0) != '"' && this.objects.locate(str) != 0) {
+		this.shared.add(str);
+	    }
+	}
+	// System.out.println("Num shared: "+shared.getNumberOfElements()+" in "+st.stopAndShow());
+
+	// Generate old predicate mapping
+	st.reset();
+	final Iterator<? extends CharSequence> itPred = this.graphs.getEntries();
+	while (itPred.hasNext()) {
+	    final CharSequence str = itPred.next();
+	    mapGraph.add(str);
+	}
+
+	// Generate old object mapping
+	final Iterator<? extends CharSequence> itObj = this.objects.getEntries();
+	while (itObj.hasNext()) {
+	    final CharSequence str = itObj.next();
+	    mapObj.add(str);
+	}
+
+	// Remove shared from subjects and objects
+	final Iterator<? extends CharSequence> itShared = this.shared.getEntries();
+	while (itShared.hasNext()) {
+	    final CharSequence sharedStr = itShared.next();
+	    this.subjects.remove(sharedStr);
+	    this.objects.remove(sharedStr);
+	}
+	// System.out.println("Mapping generated in "+st.stopAndShow());
+
+	// Sort sections individually
+	st.reset();
+	this.subjects.sort();
+	this.graphs.sort();
+	this.objects.sort();
+	this.shared.sort();
+	// System.out.println("Sections sorted in "+ st.stopAndShow());
+
+	// Update mappings with new IDs
+	st.reset();
+	for (int j = 0; j < mapSubj.size(); j++) {
+	    mapSubj.setNewID(j, this.stringToId(mapSubj.getString(j), TripleComponentRole.SUBJECT));
+	    // System.out.print("Subj Old id: "+(j+1) + " New id: "+ mapSubj.getNewID(j)+ " STR: "+mapSubj.getString(j));
+	}
+
+	for (int j = 0; j < mapGraph.size(); j++) {
+	    mapGraph.setNewID(j, this.stringToId(mapGraph.getString(j), TripleComponentRole.GRAPH));
+	    // System.out.print("Graph Old id: "+(j+1) + " New id: "+ mapPred.getNewID(j)+ " STR: "+mapPred.getString(j));
+	}
+
+	for (int j = 0; j < mapObj.size(); j++) {
+	    mapObj.setNewID(j, this.stringToId(mapObj.getString(j), TripleComponentRole.OBJECT));
+	    // System.out.print("Obj Old id: "+(j+1) + " New id: "+ mapObj.getNewID(j)+ " STR: "+mapObj.getString(j));
+	}
+	// System.out.println("Update mappings in "+st.stopAndShow());
+
+	// Replace old IDs with news
+	st.reset();
+	final Iterator<TripleID> iteratorTriples = triples.searchAll();
+	while (iteratorTriples.hasNext()) {
+	    final TripleID triple = iteratorTriples.next();
+	    triples.update(triple,
+		    mapSubj.getNewID(triple.getSubject() - 1),
+		    mapGraph.getNewID(triple.getPredicate() - 1),
+		    mapObj.getNewID(triple.getObject() - 1));
+	}
+	// System.out.println("Replace IDs in "+st.stopAndShow());
+
+	this.isOrganized = true;
+    }
+
+    @Override
+    public void startProcessing() {
+	// Do nothing.
+    }
+
+    @Override
+    public void endProcessing() {
+	// Do nothing.
+    }
+
+    @Override
+    public void close() throws IOException {
+	// Do nothing.
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.rdfhdt.hdt.dictionary.TriplesDictionary#getNsubjects()
+     */
+
+    /*
+     * (non-Javadoc)
+     * @see org.rdfhdt.hdt.dictionary.Dictionary#size()
+     */
+    @Override
+    public long size() {
+	return this.subjects.size() + this.graphs.size() + this.objects.size() + this.shared.size();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.rdfhdt.hdt.dictionary.TriplesDictionary#getNsubjects()
+     */
+    @Override
+    public long getNsubjects() {
+	return this.subjects.getNumberOfElements() + this.shared.getNumberOfElements();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.rdfhdt.hdt.dictionary.TriplesDictionary#getNpredicates()
+     */
+    @Override
+    public long getNgraphs() {
+	return this.graphs.getNumberOfElements();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.rdfhdt.hdt.dictionary.TriplesDictionary#getNobjects()
+     */
+    @Override
+    public long getNobjects() {
+	return this.objects.getNumberOfElements() + this.shared.getNumberOfElements();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.rdfhdt.hdt.dictionary.TriplesDictionary#getNshared()
+     */
+    @Override
+    public long getNshared() {
+	// TODO Auto-generated method stub
+	return this.shared.getNumberOfElements();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.rdfhdt.hdt.dictionary.Dictionary#idToString(int, org.rdfhdt.hdt.enums.TripleComponentRole)
+     */
+    @Override
+    public CharSequence idToString(final int id, final TripleComponentRole position) {
+	// TODO Auto-generated method stub
+	throw new NotImplementedException();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.rdfhdt.hdt.dictionary.Dictionary#getNumberOfElements()
+     */
+    @Override
+    public long getNumberOfElements() {
+	return this.subjects.getNumberOfElements() + this.graphs.getNumberOfElements() + this.objects.getNumberOfElements() + this.shared.getNumberOfElements();
+    }
+
+}
