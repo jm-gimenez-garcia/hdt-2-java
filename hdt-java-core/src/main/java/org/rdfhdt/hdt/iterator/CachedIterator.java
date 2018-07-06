d639cde299583e609ac2523879f8ab01714ba2e4
Jose M. Gimenez-Garcia <jm.gimenez.garcia@gmail.com>
2018-07-06 18:08:32 +0200

    iterators tests and bugfixes

--- hdt-java-core/src/main/java/org/rdfhdt/hdt/iterator/CachedIterator.java
+++ hdt-java-core/src/main/java/org/rdfhdt/hdt/iterator/CachedIterator.java
@@ -1,38 +1,44 @@
 package org.rdfhdt.hdt.iterator;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 public class CachedIterator<E> implements Iterator<E> {
 
-	Iterator<E> iterator;
-	Optional<E> cachedValue = Optional.empty();
-	
-	public CachedIterator(Iterator<E> iterator) {
-		this.iterator = iterator;
-		initialize();
-	}
-	
-	@Override
-	public boolean hasNext() {
-		return cachedValue.isPresent();
-	}
+    Iterator<E>	iterator;
+    Optional<E>	currentValue = Optional.empty();
+    Optional<E>	nextValue    = Optional.empty();
 
-	@Override
-	public E next() {
-		E next = cachedValue.get();
-		cachedValue = iterator.hasNext() ? Optional.of(iterator.next()) : Optional.empty();
-		return next;
+    public CachedIterator(final Iterator<E> iterator) {
+	this.iterator = iterator;
+	this.initialize();
+    }
+
+    @Override
+    public boolean hasNext() {
+	return this.nextValue.isPresent();
+    }
+
+    @Override
+    public E next() {
+	this.currentValue = Optional.of(this.nextValue.get());
+	this.nextValue = this.iterator.hasNext() ? Optional.of(this.iterator.next()) : Optional.empty();
+	return this.currentValue.orElseThrow(NoSuchElementException::new);
+    }
+
+    public E getCurrent() {
+	return this.currentValue.get();
+    }
+
+    public E getNext() {
+	return this.nextValue.orElseThrow(NoSuchElementException::new);
+    }
+
+    protected void initialize() {
+	if (this.iterator.hasNext()) {
+	    this.nextValue = Optional.of(this.iterator.next());
 	}
-	
-	public E current() {
-		return cachedValue.get();
-	}
-	
-	protected void initialize() {
-		if (iterator.hasNext()) {
-			cachedValue = Optional.of(iterator.next());
-		}
-	}
+    }
 
 }
--- hdt-java-core/src/main/java/org/rdfhdt/hdt/iterator/ComparableCachedIterator.java
+++ hdt-java-core/src/main/java/org/rdfhdt/hdt/iterator/ComparableCachedIterator.java
@@ -4,13 +4,13 @@ import java.util.Iterator;
 
 public class ComparableCachedIterator<E extends Comparable<E>> extends CachedIterator<E> implements Comparable<ComparableCachedIterator<E>> {
 
-	public ComparableCachedIterator(Iterator<E> iterator) {
-		super(iterator);
-	}
+    public ComparableCachedIterator(final Iterator<E> iterator) {
+	super(iterator);
+    }
 
-	@Override
-	public int compareTo(ComparableCachedIterator<E> other) {
-		return this.current().compareTo(other.current());
-	}
+    @Override
+    public int compareTo(final ComparableCachedIterator<E> other) {
+	return this.getNext().compareTo(other.getNext());
+    }
 
 }
