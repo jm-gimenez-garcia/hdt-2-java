bb9145a281599e3849901519f5f119b8410914b1
AlyHdr <ali.haidar-97@hotmail.com>
2019-07-06 01:15:34 +0200

    Add cat to BitmapQuads.java

--- hdt-java-core/src/main/java/org/rdfhdt/hdt/triples/impl/BitmapQuads.java
+++ hdt-java-core/src/main/java/org/rdfhdt/hdt/triples/impl/BitmapQuads.java
@@ -27,10 +27,14 @@
 
 package org.rdfhdt.hdt.triples.impl;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -43,6 +47,7 @@ import org.rdfhdt.hdt.compact.permutation.Permutation;
 import org.rdfhdt.hdt.compact.permutation.PermutationFactory;
 import org.rdfhdt.hdt.compact.sequence.Sequence;
 import org.rdfhdt.hdt.compact.sequence.SequenceLog64Big;
+import org.rdfhdt.hdt.compact.sequence.SequenceLog64BigDisk;
 import org.rdfhdt.hdt.enums.TripleComponentOrder;
 import org.rdfhdt.hdt.enums.TripleComponentRole;
 import org.rdfhdt.hdt.exceptions.IllegalFormatException;
@@ -50,6 +55,7 @@ import org.rdfhdt.hdt.hdt.HDTVocabulary;
 import org.rdfhdt.hdt.header.Header;
 import org.rdfhdt.hdt.listener.ProgressListener;
 import org.rdfhdt.hdt.options.ControlInfo;
+import org.rdfhdt.hdt.options.ControlInformation;
 import org.rdfhdt.hdt.options.HDTOptions;
 import org.rdfhdt.hdt.options.HDTSpecification;
 import org.rdfhdt.hdt.triples.IteratorTripleID;
@@ -282,5 +288,120 @@ public class BitmapQuads extends BitmapTriples {
 		super.close();
 		this.permutation = null;
 	}
+	 public void cat(String location,IteratorTripleID it, ProgressListener listener){
+	        try {
+	            long number = it.estimatedNumResults();
+	            SequenceLog64BigDisk vectorY = new SequenceLog64BigDisk(location + "vectorY", BitUtil.log2(number), number);
+	            SequenceLog64BigDisk vectorZ = new SequenceLog64BigDisk(location + "vectorZ",BitUtil.log2(number), number);
+	            ModifiableBitmap bitY = new Bitmap375();//Disk(location + "bitY",number);
+	            ModifiableBitmap bitZ = new Bitmap375();//Disk(location + "bitZ",number);
+	            
+	            final SequenceLog64BigDisk vectorG = new SequenceLog64BigDisk(location + "vectorG",BitUtil.log2(number), number);
+	            
+	    		final ModifiableBitmap bitG = new Bitmap375(number);
+	            
+	    		long lastX=0, lastY=0, lastZ=0;
+	            long x, y, z;
+	            long numTriples=0;
+	            long g;
+	            while(it.hasNext()) {
+	                TripleID triple = it.next();
+	                TripleOrderConvert.swapComponentOrder(triple, TripleComponentOrder.SPO, TripleComponentOrder.SPO);
 
+	                x = triple.getSubject();
+	                y = triple.getPredicate();
+	                z = triple.getObject();
+	    			g = triple instanceof QuadID ? toRoleID(((QuadID) triple).getGraph(), TripleComponentRole.GRAPH) : 0;
+
+	                if(x==0 || y==0 || z==0) {
+	                    throw new IllegalFormatException("None of the components of a triple can be null");
+	                }
+
+	                if(numTriples==0) {
+	                    // First triple
+	                    vectorY.append(y);
+	                    vectorZ.append(z);
+	                } else if(x!=lastX) {
+	                    if(x!=lastX+1) {
+	                        throw new IllegalFormatException("Upper level must be increasing and correlative.");
+	                    }
+	                    // X changed
+	                    bitY.append(true);
+	                    vectorY.append(y);
+
+	                    bitZ.append(true);
+	                    vectorZ.append(z);
+	                } else if(y!=lastY) {
+	                    if(y<lastY) {
+	                        throw new IllegalFormatException("Middle level must be increasing for each parent.");
+	                    }
+
+	                    // Y changed
+	                    bitY.append(false);
+	                    vectorY.append(y);
+
+	                    bitZ.append(true);
+	                    vectorZ.append(z);
+	                } else {
+	                    if(z<lastZ) {
+	                        throw new IllegalFormatException("Lower level must be increasing for each parent.");
+	                    }
+
+	                    // Z changed
+	                    bitZ.append(false);
+	                    vectorZ.append(z);
+	                }
+	                if (g == 0) {
+	    				bitG.append(false);
+	    			} else
+	    			{
+	    				bitG.append(true);
+	    				vectorG.append(g);
+	    			}
+
+	                lastX = x;
+	                lastY = y;
+	                lastZ = z;
+
+	                ListenerUtil.notifyCond(listener, "Converting to BitmapTriples", numTriples, numTriples, number);
+	                numTriples++;
+	            }
+
+	            if(numTriples>0) {
+	                bitY.append(true);
+	                bitZ.append(true);
+	            }
+
+	            vectorY.aggressiveTrimToSize();
+	            vectorZ.trimToSize();
+
+	            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(location + "triples"));
+	            ControlInfo ci = new ControlInformation();
+	            ci.setType(ControlInfo.Type.TRIPLES);
+	            ci.setFormat(HDTVocabulary.TRIPLES_TYPE_BITMAP);
+	            ci.setInt("order", TripleComponentOrder.SPO.ordinal());
+	            ci.setType(ControlInfo.Type.TRIPLES);
+	            ci.save(bos);
+	            
+	            IntermediateListener iListener = new IntermediateListener(listener);
+	            
+	            bitY.save(bos, iListener);
+	            bitZ.save(bos, iListener);
+	            bitG.save(bos, iListener);
+	            
+	            vectorY.save(bos, iListener);
+	            vectorZ.save(bos, iListener);
+	            vectorG.save(bos, iListener);
+	            
+	            Files.delete(Paths.get(location + "vectorY"));
+	            Files.delete(Paths.get(location + "vectorZ"));
+	            Files.delete(Paths.get(location + "vectorG"));
+	            
+	            //Files.delete(Paths.get(location + "bitY"));
+	            //Files.delete(Paths.get(location + "bitZ"));
+	            bos.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 }
